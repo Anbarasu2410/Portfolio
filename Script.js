c8777212cf6a0f3237ddfaafdb20853865945d03
Anbarasu <96944725+Anbarasu2410@users.noreply.github.com>
2025-09-26 13:48:36 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,10 @@
+// Show sections on scroll
+const sections = document.querySelectorAll("section");
+window.addEventListener("scroll", () => {
+  sections.forEach(section => {
+    const top = section.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      section.classList.add("visible");
+    }
+  });
+});
